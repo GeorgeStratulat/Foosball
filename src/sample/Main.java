fb0ef6c6552d3ea065e3fb29f75cb602291b68f8
tibbiustin <tibe.kinda@yahoo.ro>
2017-04-24 10:11:21 +0200

    generate matches

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("FirstWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Match.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1024, 720));
         primaryStage.show();
